2eec7ff4cfcc66c0e762143f5e6086f4819c9a50
ChrisB007 <chrisbitoy@gmail.com>
2021-04-26 10:03:36 -0400

    latest update

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,7 @@ import {Link} from 'react-scroll';
 const features = [
   {
     name: 'Garage-API',
-    description: 'API platform to host all of my RESTful API that I built and launched',
+    description: 'API platform to host all of my RESTful API that I developed',
     image: "/images/api.png",
     github: 'url',
     website: 'url'
